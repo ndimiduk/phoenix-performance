5b69a64fa1e050facb1491fc7bcfae5e13afff07
Hue <hue@sandbox.hortonworks.com>
2014-11-02 06:27:31 -0800

    Create tables before bulk load.

---
+++ ddl/CreateTables.sql
@@ -0,0 +1,64 @@
+drop table if exists date_dim;
+drop table if exists store_sales;
+
+create table store_sales
+(
+    ss_sold_date_sk           integer,
+    ss_sold_time_sk           integer,
+    ss_item_sk                integer not null,
+    ss_customer_sk            integer,
+    ss_cdemo_sk               integer,
+    ss_hdemo_sk               integer,
+    ss_addr_sk                integer,
+    ss_store_sk               integer,
+    ss_promo_sk               integer,
+    ss_ticket_number          integer not null,
+    ss_quantity               integer,
+    ss_wholesale_cost         float,
+    ss_list_price             float,
+    ss_sales_price            float,
+    ss_ext_discount_amt       float,
+    ss_ext_sales_price        float,
+    ss_ext_wholesale_cost     float,
+    ss_ext_list_price         float,
+    ss_ext_tax                float,
+    ss_coupon_amt             float,
+    ss_net_paid               float,
+    ss_net_paid_inc_tax       float,
+    ss_net_profit             float,
+    ss_sold_date varchar,
+    constraint ss_pk primary key (ss_item_sk, ss_ticket_number)
+);
+
+create table date_dim
+(
+    d_date_sk                 integer               not null,
+    d_date_id                 char(16)              not null,
+    d_date                    date                          ,
+    d_month_seq               integer                       ,
+    d_week_seq                integer                       ,
+    d_quarter_seq             integer                       ,
+    d_year                    integer                       ,
+    d_dow                     integer                       ,
+    d_moy                     integer                       ,
+    d_dom                     integer                       ,
+    d_qoy                     integer                       ,
+    d_fy_year                 integer                       ,
+    d_fy_quarter_seq          integer                       ,
+    d_fy_week_seq             integer                       ,
+    d_day_name                char(9)                       ,
+    d_quarter_name            char(6)                       ,
+    d_holiday                 char(1)                       ,
+    d_weekend                 char(1)                       ,
+    d_following_holiday       char(1)                       ,
+    d_first_dom               integer                       ,
+    d_last_dom                integer                       ,
+    d_same_day_ly             integer                       ,
+    d_same_day_lq             integer                       ,
+    d_current_day             char(1)                       ,
+    d_current_week            char(1)                       ,
+    d_current_month           char(1)                       ,
+    d_current_quarter         char(1)                       ,
+    d_current_year            char(1)                       ,
+    constraint d_pk primary key (d_date_sk, d_date_id)
+);
