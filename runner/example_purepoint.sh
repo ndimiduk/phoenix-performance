6ff5ff2645c4e70956aa14134df3e5a24adf3b3e
hbase <hbase@cluster1.example.com>
2014-12-06 22:34:52 -0500

    New tests, switch parsing to XML to track queries, fix setup.

--- runner/example_purepoint.sh
+++ runner/example_purepoint.sh
@@ -6,4 +6,5 @@ python runTest.py \
 	-v VAR_REQUESTS_PER_THREAD=1500 \
 	-v VAR_NUM_THREADS=4 \
 	-v VAR_RAMP_TIME=1 \
+	-v VAR_POOL_MAX=4 \
 	-v VAR_ZOOKEEPER_QUORUM=cluster1:2181:/hbase-unsecure
---
+++ runner/example_storedayaggnogby.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Semi point lookups.
+python runTest.py \
+	-i ../jmeter_tests/Phoenix_StoreSales_StoreDayAggNoGby.jmx \
+	-v VAR_REQUESTS_PER_THREAD=3 \
+	-v VAR_NUM_THREADS=4 \
+	-v VAR_RAMP_TIME=1 \
+	-v VAR_POOL_MAX=4 \
+	-v VAR_ZOOKEEPER_QUORUM=cluster1:2181:/hbase-unsecure
