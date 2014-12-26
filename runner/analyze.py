efb3494a51e175f17d769ddafd4dec8bc9f729c1
Carter Shanklin <carter@hw10204.local>
2014-12-26 14:35:59 -0800

    More use of carbon

--- runner/analyze.py
+++ runner/analyze.py
@@ -1,8 +1,10 @@
 import xml.parsers.expat
 import csv
 import numpy
+import getopt
 import glob
 import re
+import socket
 import sys
 import datetime
 
@@ -15,6 +17,13 @@ bytes=[]
 latency=[]
 minTimestamp = sys.maxint
 maxTimestamp = 0
+
+def netcat(hostname, port, content):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((hostname, port))
+	s.sendall(content)
+	s.shutdown(socket.SHUT_WR)
+	s.close()
 
 def outputMeasure(name, values, totalReq, failRatio):
 	nv = numpy.array(values)
@@ -58,63 +67,97 @@ def start_element(name, attributeMap):
 	if ts > maxTimestamp:
 		maxTimestamp = ts
 
-# For stripping things that will cause the XML parser to choke.
-control_chars = ''.join(map(unichr, [38] + range(127,256)))
-control_char_re = re.compile('[%s]' % re.escape(control_chars))
+def process():
+	global rowCount, failCount
+	global timestamp, elapsed, bytes, latency
+	global minTimestamp, maxTimestamp
+	results = []
 
-csvs = glob.glob("output.csv.*")
-xmls = glob.glob("output.xml.*")
+	# For stripping things that will cause the XML parser to choke.
+	control_chars = ''.join(map(unichr, [38] + range(127,256)))
+	control_char_re = re.compile('[%s]' % re.escape(control_chars))
 
-for xml in xmls:
-	with open(xml) as fd:
-		text = fd.read()
-		fd.close()
-		stripped = control_char_re.sub('X', text)
-		p = xml.parsers.expat.ParserCreate()
-		p.StartElementHandler = start_element
-		p.Parse(stripped)
+	csvs = glob.glob("output.csv.*")
+	xmls = glob.glob("output.xml.*")
 
-for csvf in csvs:
-	with open(csvf) as fd:
-		r = csv.reader(fd)
-		next(r, None) 
-		for row in r:
-			rowCount = rowCount + 1
-			elapsed.append(int(row[1]))
-			bytes.append(int(row[5]))
-			latency.append(int(row[6]))
-			if row[4] == "false":
-				failCount = failCount + 1
-			ts = int(row[0])
-			timestamp.append(ts)
-			if ts < minTimestamp:
-				minTimestamp = ts
-			if ts > maxTimestamp:
-				maxTimestamp = ts
+	for xml in xmls:
+		with open(xml) as fd:
+			text = fd.read()
+			fd.close()
+			stripped = control_char_re.sub('X', text)
+			p = xml.parsers.expat.ParserCreate()
+			p.StartElementHandler = start_element
+			p.Parse(stripped)
 
-failRatio = "%0.2f" % (100.0 * failCount / rowCount)
-print "Measure,Min,Max,Median,90,95,99,99.9,TotalReq,FailRatio"
-outputMeasure("elapsed", elapsed, rowCount, failRatio)
-outputMeasure("bytes", bytes, rowCount, failRatio)
-outputMeasure("latency", latency, rowCount, failRatio)
-print
+	for csvf in csvs:
+		with open(csvf) as fd:
+			r = csv.reader(fd)
+			next(r, None) 
+			for row in r:
+				rowCount = rowCount + 1
+				elapsed.append(int(row[1]))
+				bytes.append(int(row[5]))
+				latency.append(int(row[6]))
+				if row[4] == "false":
+					failCount = failCount + 1
+				ts = int(row[0])
+				timestamp.append(ts)
+				if ts < minTimestamp:
+					minTimestamp = ts
+				if ts > maxTimestamp:
+					maxTimestamp = ts
 
-# Calculate the requests per second on 5 second boundaries.
-timestamp.sort()
-timestamp = [ x/1000 for x in timestamp ]
-minTime = timestamp[0]
-maxTime = timestamp[-1]
-blockMin = ((minTime / 5) * 5)
-blockMax = blockMin + 5
-ts = numpy.asarray(timestamp)
-print "StartTime,EndTime,RPS"
-while blockMin < maxTime:
-	mint = datetime.datetime.fromtimestamp(blockMin).strftime('%Y-%m-%d %H:%M:%S')
-	maxt = datetime.datetime.fromtimestamp(blockMax).strftime('%Y-%m-%d %H:%M:%S')
-	rps = str(((ts <= blockMax).sum() - (ts < blockMin).sum()) * 1.0 / 5)
-	print ",".join([mint, maxt, rps])
-	blockMin = blockMax
-	blockMax = blockMax + 5
+	failRatio = "%0.2f" % (100.0 * failCount / rowCount)
+	print "Measure,Min,Max,Median,90,95,99,99.9,TotalReq,FailRatio"
+	outputMeasure("elapsed", elapsed, rowCount, failRatio)
+	outputMeasure("bytes", bytes, rowCount, failRatio)
+	outputMeasure("latency", latency, rowCount, failRatio)
+	print
 
-totalTime = (maxTimestamp - minTimestamp) / 1000.0
-#print "Total requests per second = %02f" % ((rowCount * 1.0) / (totalTime))
+	# Calculate the requests per second on 10 second boundaries.
+	boundary = 10
+	timestamp.sort()
+	timestamp = [ x/1000 for x in timestamp ]
+	minTime = timestamp[0]
+	maxTime = timestamp[-1]
+	blockMin = ((minTime / boundary) * boundary)
+	blockMax = blockMin + boundary
+	ts = numpy.asarray(timestamp)
+	while blockMin < maxTime:
+		maxt = datetime.datetime.fromtimestamp(blockMax).strftime("%s")
+		rps = ((ts <= blockMax).sum() - (ts < blockMin).sum()) * 1.0 / boundary
+		value = "phoenix.opspersec.cluster %f %s" % (rps, maxt)
+		#print value
+		results.append(value)
+		blockMin = blockMax
+		blockMax = blockMax + boundary
+
+	totalTime = (maxTimestamp - minTimestamp) / 1000.0
+	#print "Total requests per second = %02f" % ((rowCount * 1.0) / (totalTime))
+	return results
+
+def main():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "c:", ["help", "output="])
+	except getopt.GetoptError as err:
+		print str(err)
+		print "Arguments: -c carbon"
+		print "Ex: analyze.py -c 192.168.129.147:2003"
+		sys.exit(2)
+
+	carbonHost = None
+	carbonPort = None
+	for o, a in opts:
+		if o == "-c":
+			(carbonHost, carbonPort) = a.split(":")
+			carbonPort = int(carbonPort)
+		else:
+			assert False, "unhandled option"
+
+	results = process()
+	rtext = "\n".join(results)
+	print rtext
+	if carbonHost:
+		netcat(carbonHost, carbonPort, rtext)
+
+main()
--- runner/hbaseStats.py
+++ runner/hbaseStats.py
@@ -3,9 +3,20 @@
 import datetime
 import getopt
 import json
+import operator
+import re
 import requests
+import socket
 import sys
 import time
+import urllib
+
+def netcat(hostname, port, content):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((hostname, port))
+	s.sendall(content)
+	s.shutdown(socket.SHUT_WR)
+	s.close()
 
 def findBean(beans, name):
 	for bean in beans:
@@ -39,6 +50,164 @@ def printProperties(props):
 	for k in keys:
 		ary.append(str(props[k]))
 	print ",".join(ary)
+
+def newPoll(hmaster):
+	# Load info from the HMaster.
+	hmasterJmx = "http://" + hmaster + "/jmx"
+	targetMasterBeanName = "Hadoop:service=HBase,name=Master,sub=Server"
+	r = requests.get(hmasterJmx)
+	rawJson = r.text
+	decoded = json.loads(rawJson)
+	targetBean = findBean(decoded["beans"], targetMasterBeanName)
+
+	regionBean = 'Hadoop:service=HBase,name=RegionServer,sub=Regions'
+	excludeBean = [
+		'JMImplementation:type=MBeanServerDelegate',
+		'java.lang',
+		'java.util.logging',
+		'com.sun.management',
+		# regionBean,
+	]
+	excludeTable = [
+		'kylin',
+	]
+
+	# Iterate through all live RegionServers.
+	results = []
+	liveRegionServers = targetBean["tag.liveRegionServers"]
+	regionServers = liveRegionServers.split(";")
+	regionRe = "[Nn]amespace_([^_]+)_table_(.+?)_region_[0-9a-f]+_metric_(.+)"
+	tableStats = {}
+	for r in regionServers:
+		components = r.split(",")
+		host = components[0]
+		endpoint = "http://" + host + ":60030/jmx"
+		rsStats = requests.get(endpoint)
+		rsJson = rsStats.text
+		statJson = json.loads(rsJson)
+
+		now = int(time.time())
+		beans = statJson["beans"]
+		for bean in beans:
+			beanName = bean["name"]
+			if len(filter(lambda x: beanName.find(x) > -1, excludeBean)) > 0:
+				continue
+			#beanName = urllib.quote_plus(beanName)
+			if beanName == regionBean:
+				for property in bean.keys():
+					val = bean[property]
+					m = re.match(regionRe, property)
+					if m:
+						(namespace, table, stat) = (m.group(1), m.group(2), m.group(3))
+						if len(filter(lambda x: table.find(x) > -1, excludeTable)) > 0:
+							continue
+						fqTable = "%s.%s" % (namespace, table)
+						if tableStats.has_key(fqTable):
+							tableStats[fqTable].append([stat, val])
+						else:
+							tableStats[fqTable] = [[stat, val]]
+					else:
+						pass
+				pass
+			else:
+				for property in bean.keys():
+					if property == "name":
+						continue
+					val = bean[property]
+					if isinstance(val, int) or isinstance(val, float):
+						stat = "hbase.%s.%s %s %d" % (host, property, val, now)
+						results.append(stat)
+
+	rollupTableStats(results, tableStats)
+	return results
+
+def meanOfMeans(means, numbers):
+	total = sum(numbers)
+	if total == 0:
+		return 0
+	return sum(map(operator.mul, means, numbers)) * 1.0 / total
+
+# Note: This is just an estimate.
+def medianOfMedians(list):
+	list.sort()
+	return list[len(list)/2]
+
+def variance(list):
+	average = sum(list) / len(list)
+	variance = sum((average - value) ** 2 for value in list) / len(list)
+	return variance
+
+def rollupTableStats(results, tableStats):
+	stats = {}
+	regionMetrics = {}
+
+	operations = {
+		"appendCount"			: sum,
+		"compactionsCompletedCount"	: sum,
+		"deleteCount"			: sum,
+		"get_75th_percentile"		: max,
+		"get_95th_percentile"		: max,
+		"get_99th_percentile"		: max,
+		"get_max"			: max,
+		"get_mean"			: "special",
+		"get_median"			: medianOfMedians,
+		"get_min"			: min,
+		"get_num_ops"			: sum,
+		"incrementCount"		: sum,
+		"memStoreSize"			: sum,
+		"mutateCount"			: sum,
+		"numBytesCompactedCount"	: sum,
+		"numFilesCompactedCount"	: sum,
+		"scanNext_75th_percentile"	: max,
+		"scanNext_95th_percentile"	: max,
+		"scanNext_99th_percentile"	: max,
+		"scanNext_max"			: max,
+		"scanNext_mean"			: "special",
+		"scanNext_median"		: medianOfMedians,
+		"scanNext_min"			: min,
+		"scanNext_num_ops"		: sum,
+		"storeCount"			: sum,
+		"storeFileCount"		: sum,
+		"storeFileSize"			: sum,
+		"get_num_var"			: "special",
+		"scanNext_num_var"		: "special",
+	}
+
+	for table in tableStats.keys():
+		stats[table] = {}
+		regionMetrics[table] = []
+		regionStats = tableStats[table]
+
+		for o in operations:
+			function = operations[o]
+			if function == "special":
+				continue
+			hits = filter(lambda x: x[0] == o, regionStats)
+			values = [ x[1] for x in hits ]
+			stats[table][o] = function(values)
+
+		# Special values.
+		numOps = filter(lambda x: x[0] == "get_num_ops", regionStats)
+		numOps = [ x[1] for x in numOps ]
+		stats[table]["get_num_var"] = variance(numOps)
+		gmeans = filter(lambda x: x[0] == "get_mean", regionStats)
+		gmeans = [ x[1] for x in gmeans ]
+		stats[table]["get_mean"] = meanOfMeans(gmeans, numOps)
+		numOps = filter(lambda x: x[0] == "scanNext_num_ops", regionStats)
+		numOps = [ x[1] for x in numOps ]
+		stats[table]["scanNext_num_var"] = variance(numOps)
+		smeans = filter(lambda x: x[0] == "scanNext_mean", regionStats)
+		smeans = [ x[1] for x in smeans ]
+		stats[table]["scanNext_mean"] = meanOfMeans(smeans, numOps)
+
+	# Add the stats in.
+	now = int(time.time())
+	for table in tableStats.keys():
+		for met in operations.keys():
+			metric = "%s.%s" % (table, met)
+			val = stats[table][met]
+			stat = "hbase.table.%s %s %d" % (metric, val, now)
+			results.append(stat)
 
 def poll(hmaster):
 	hmasterJmx = "http://" + hmaster + "/jmx"
@@ -106,30 +275,44 @@ def poll(hmaster):
 
 def main():
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "m:s:", ["help", "output="])
+		opts, args = getopt.getopt(sys.argv[1:], "b:c:m:", ["help", "output="])
 	except getopt.GetoptError as err:
 		print str(err)
+		print "Arguments: -m hmaster -b boundary -c carbon"
+		print "Ex: hbaseStats.py -m 192.168.129.137:60010 -c 192.168.129.147:2003"
 		sys.exit(2)
 
 	hmaster = None
-	sleepTime = 5
+	boundary = 10
+	carbonHost = None
+	carbonPort = None
 	for o, a in opts:
 		if o == "-m":
 			hmaster = a
-		elif o == "-s":
-			sleepTime = int(a)
+		elif o == "-b":
+			boundary = int(a)
+		elif o == "-c":
+			(carbonHost, carbonPort) = a.split(":")
+			carbonPort = int(carbonPort)
 		else:
 			assert False, "unhandled option"
 
 	if hmaster == None:
-		assert False, "Need HMaster (-m)"
+		assert False, "Need HMaster (ex: -m 192.168.129.137:60010)"
 
 	while True:
-		# Wait for an appropriate start time (5s boundary).
-		sometime = (int(time.time() / 5) * 5) + 5 - time.time()
+		# Wait for the boundary time.
+		sometime = (int(time.time() / boundary) * boundary) + boundary - time.time()
 		time.sleep(sometime)
 
 		# Poll all regionservers (serially)
-		poll(hmaster)
+		results = newPoll(hmaster)
+		rtext = "\n".join(results)
+		if carbonHost:
+			print "Sending stats to carbon"
+			print rtext
+			netcat(carbonHost, carbonPort, rtext)
+		else:
+			print rtext
 
 main()
